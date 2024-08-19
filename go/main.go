ff8172555d24e7007c7f1f112d98b66278160c14
aster <137767097+aster-void@users.noreply.github.com>
2024-08-19 14:42:46 +0900

    improve go io

--- go/main.go
+++ go/main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+
 	fizzbuzz "github.com/aster-void/fizzbuzz/go/app"
 )
 
 func main() {
-	list := fizzbuzz.Range(1, 15)
+	var max, min int = 15, 1
+	switch len(os.Args) {
+	case 1:
+		// noop
+	case 2:
+		var err error
+		max, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalln("Failed to parse args:", os.Args[1])
+		}
+	case 3:
+		var err error
+		min, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalln("Failed to parse args:", os.Args[1])
+		}
+		max, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatalln("Failed to parse args:", os.Args[2])
+		}
+	default:
+		log.Fatalln("too many args provided. args:", os.Args[1:])
+	}
+
+	if max < min {
+		log.Fatalln("invalid args: max >= min \nmax =", max, "\nmin =", min)
+	}
+
+	list := fizzbuzz.Range(min, max)
 	for _, item := range list {
 		fmt.Println(item)
 	}
